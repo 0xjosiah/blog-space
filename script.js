0c4b75e6c55e336d72027458246c3d551b20d69d
josiahwebb <jj.webb520@gmail.com>
2022-03-24 21:10:58 -0600

    allows for user to post, post shows up most recent

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
+const newPost = document.querySelector('form')
+
 fetch('https://apis.scrimba.com/jsonplaceholder/posts')
     .then(response => response.json())
-    .then(data => {
-        const blogData = data.slice(0,5)
-        for (let i = 0; i < blogData.length; i++) {
-            let div = document.createElement('div')
-            let blogHtml = `<h3>${blogData[i].title}</h3>
-                            <p>${blogData[i].body}</p>`
-            div.innerHTML = blogHtml
-            document.body.appendChild(div)
-        }
-    })
+    .then(data => displayFetchedBlogs(data, 8))
+    
+function displayFetchedBlogs(data, numOfBlogs) {
+    const dataArr = data.slice(0,numOfBlogs)
+    dataArr.forEach(i => displayNewBlog(i))
+}
 
+function displayNewBlog(blogObj) {
+    let div = document.createElement('div')
+    let blogHtml = `<h3>${blogObj.title}</h3>
+                    <p>${blogObj.body}</p>`
+    div.innerHTML = blogHtml
+    let mostRecentPost = document.querySelectorAll('div')[1]
+    document.querySelector('section').insertBefore(div, mostRecentPost)
+}
+
+newPost.addEventListener('submit', event => {
+    event.preventDefault()
+    const blogData = {
+        title: document.querySelector('#new-blog-title').value,
+        body: document.querySelector('#new-blog-body').value
+    }
+    fetch('https://apis.scrimba.com/jsonplaceholder/posts', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(blogData)
+    })
+        .then(res => res.json())
+        .then(data => {
+            displayNewBlog(data)
+            newPost.reset()
+        })
+})
